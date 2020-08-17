fc481861e31028adec1b688e0f09c2a5a11d74fa
sakibalamin <sakibalamin162@gmail.com>
2020-08-17 13:49:12 -0700

    added cf1392A

---
+++ Codeforces/cf1392A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define SZ(v) (int)v.size()
+#define endl "\n"
+#define ALL(v) v.begin(), v.end()
+#define UNIQUE(v) sort(ALL(v)),v.erase(unique(ALL(v)),v.end())
+typedef long long LL;
+typedef pair <int,int> pii;
+const double pi = acos(-1.0);
+const double eps = 1e-8;
+const int inf = 1e9;
+const LL INF = 1e18;
+const int mod = 1e9 + 7;
+const int N = 1e5 + 5;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector <LL> a (n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    sort(a, a + n);
+    if (a[0] == a[n - 1]) cout << n << endl;
+    else cout << 1 << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    #ifdef LOCAL
+        freopen("input.txt", "r", stdin);
+    #endif
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) solve();
+    return 0;
+}
